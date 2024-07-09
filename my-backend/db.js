3a92219747ecfb07cbfa2bcc732097611a72af5f
Sutiksh <sutiksh_mishra@gkcodelabs.com>
2024-07-09 13:59:00 +0530

    Updated

--- my-backend/db.js
+++ my-backend/db.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: `postgres://postgres:${encodeURIComponent("samsung@1109")}@localhost:5432/gkc_authorization`
+  connectionString: `postgres://postgres:${encodeURIComponent("Sutiksh@17")}@localhost:5432/gkc_auth`
 });
 module.exports = pool;
